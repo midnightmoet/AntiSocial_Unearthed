20499ef79b29e98edbf1d445fe5c7b7fcc2391ab
Lisa Miner <midnightmoet@gmail.com>
2023-10-20 17:08:12 -0400

    Starting to make the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,24 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material";
-import { themeSettings } from "theme";
+import { themeSettings } from "./theme";
 
 function App() {
-  const mode = useSelector((state) => state.theme.mode);
+  const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
       <BrowserRouter>
+        <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile/:userId" element={<ProfilePage />} />          
           <Route path="/*" element={<NotFound />} />
         </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
