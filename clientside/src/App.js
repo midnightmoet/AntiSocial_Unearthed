4a68e2d9bc2670da795b9095974c6aa462dc5cb0
Lisa Miner <midnightmoet@gmail.com>
2023-10-31 14:32:29 -0400

    Working on this today

---
+++ clientside/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
+const App = () => {
+
+  return (
+    <div className='app'>
+      <h1>AntiSocial</h1>
+    </div>
+  )
+}
+
+export default App
